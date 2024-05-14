57a137c62bb4994db4f9aa2361dc513aa4d29590
Danil Golota <info@shalotts.site>
2024-05-14 19:45:32 +0500

    feat: import, remove, dnd

--- app/client/main.js
+++ app/client/main.js
@@ -14,26 +14,30 @@ const buttonPoint = (map) => {
         const coords = e.get('coords')
         const placemark = new ymaps.Placemark(coords, {
           iconContent: 'текст',
-          draggable: true
+          hintContent: '',
+          balloonContent: '',
         }, {
-          preset: 'islands#darkOrangeStretchyIcon'
+          preset: 'islands#darkOrangeStretchyIcon',
+          draggable: true
         });
 
         placemark.events.add('contextmenu', (e) => {
           const menu = document.getElementById('menu')
-          console.log(menu)
 
           if (menu) {
             menu.remove()
           } else {
             const menuContent = document.createElement('div')
             menuContent.id = 'menu'
-            menuContent.innerHTML = '<ul id="menu_list">\
-                      <li>Name: <br /> <input type="text" name="icon_text" /></li>\
-                      <li>Hint: <br /> <input type="text" name="hint_text" /></li>\
-                      <li>Balloon: <br /> <input type="text" name="balloon_text" /></li>\
+            menuContent.innerHTML = '<ul id="menu_list" xmlns="http://www.w3.org/1999/html">\
+                      <li>Номер палатки: <br /> <input type="text" name="icon_text" /></li>\
+                      <li>Подсказка: <br /> <input type="text" name="hint_text" /></li>\
+                      <li>Описание: <br /> <input type="text" name="balloon_text" /></li>\
                   </ul>\
-              <div align="center"><input type="submit" value="Save" /></div> ';
+               <div style="display: flex; gap: 10px;">\
+               <button name="remove" type="button" >Удалить</button>\
+               <button type="submit" >Сохранить</button>\
+               </div>';
 
 
             document.body.append(menuContent)
@@ -51,7 +55,12 @@ const buttonPoint = (map) => {
             hintText.value = placemark.properties.get('hintContent')
             balloonText.value = placemark.properties.get('balloonContent')
 
-            document.getElementById('menu').querySelector('input[type="submit"]').addEventListener('click', () => {
+            document.querySelector('button[name="remove"]').addEventListener('click', () => {
+              map.geoObjects.remove(placemark)
+              document.getElementById('menu').remove();
+            })
+
+            document.getElementById('menu').querySelector('button[type="submit"]').addEventListener('click', () => {
               placemark.properties.set({
                 iconContent: iconText.value,
                 hintContent: hintText.value,
@@ -71,6 +80,71 @@ const buttonPoint = (map) => {
   return button
 }
 
+const buttonGEOJSON = (map, objectManager) => {
+  const button = new ymaps.control.Button('<b>GEOJSON</b>');
+
+  button.events.add('click', function () {
+    if (document.querySelector('.modal')) {
+      document.querySelector('.modal').remove()
+    } else {
+      const modal = document.createElement('div')
+      modal.className = 'modal'
+      modal.id = 'geoJson'
+      modal.innerHTML = `<form>
+       <input name="upload" type="file" />
+       <button type="button" name="export">Экспорт</button>
+    </form>`
+
+      document.body.append(modal)
+
+      modal.querySelector('button[name="export"]').addEventListener('click', () => {
+        modal.remove()
+      })
+
+      modal.querySelector('input[name="upload"]').addEventListener('change', (e) => {
+        const file = e.target.files[0]
+        const reader = new FileReader();
+
+        reader.readAsText(file);
+
+        reader.onload = function() {
+          try {
+            const json = JSON.parse(reader.result)
+            importGEOJSON(json, objectManager, map)
+          } catch (e) {
+            console.log(e)
+            alert('Не удалось импортировать файл')
+          }
+        };
+
+        reader.onerror = function() {
+          console.log(reader.error);
+          alert('Не удалось прочитать файл')
+        };
+
+        modal.remove()
+      })
+    }
+  })
+
+  return button
+}
+
+const importGEOJSON = (geoJson, objectManager, map) => {
+  geoJson.features.forEach(function (obj) {
+    obj.properties.balloonContent = obj.properties.description;
+
+    if (obj.properties.iconCaption) {
+      obj.options = {
+        preset: "islands#greenDotIconWithCaption"
+      }
+    }
+  });
+
+  objectManager.add(geoJson);
+  map.geoObjects.add(objectManager);
+}
+
 function init(){
   console.log(ymaps.control)
 
@@ -79,8 +153,11 @@ function init(){
     zoom: 19,
     controls: ['zoomControl', 'searchControl', 'typeSelector']
   });
+  const objectManager = new ymaps.ObjectManager()
 
-  const button = buttonPoint(map)
-  map.controls.add(button)
+  const point = buttonPoint(map)
+  const geoJson = buttonGEOJSON(map, objectManager)
+  map.controls.add(point)
+  map.controls.add(geoJson)
 }
 
