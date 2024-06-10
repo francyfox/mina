6183310cc580f639d70f4ff57151a971dcb496e1
Danil Golota <info@shalotts.site>
2024-06-10 16:29:35 +0500

    feat: change db

--- nuxt/app/module/db/db.firebase.js
+++ nuxt/app/module/db/db.firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { collection, getFirestore } from 'firebase/firestore'
 
 export const firebaseConfig = {
-  apiKey: 'AIzaSyA_3VMW28JFgz5KGdUKUucsRFFIdvLmarw',
-  authDomain: 'mina-gorodok.firebaseapp.com',
-  projectId: 'mina-gorodok',
-  storageBucket: 'mina-gorodok.appspot.com',
-  messagingSenderId: '369463332361',
-  appId: '1:369463332361:web:04ab194fd851fb8241b2d8'
+  apiKey: "AIzaSyDAkA0LLaOmJRgi2eD6hVA6Cte1AcK5YR0",
+  authDomain: "marwa-b0eb8.firebaseapp.com",
+  projectId: "marwa-b0eb8",
+  storageBucket: "marwa-b0eb8.appspot.com",
+  messagingSenderId: "1064049645860",
+  appId: "1:1064049645860:web:40d8b54043a774dc1b7bb1"
 }
 export const firebase = initializeApp(firebaseConfig)
 export const db = getFirestore(firebase)
